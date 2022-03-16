e00b5617e16044924a9d2d45dddd8cd99424bb89
Manoj-s18 <sunkadmanu@gmail.com>
2022-03-16 10:25:57 +0530

    Created an Expense tracker , Initial commit created the UI and Expense components that display Expense Data[Static Data]

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Expenses from './components/Expense/Expenses';
 function App() {
+  
+  const expenses = [
+    {
+      id:1,
+      title:'car service',
+      amount:'50,000',
+      date : new Date(2019,4,25)
+    },
+    {
+      id:2,
+      title:'Birthday Party',
+      amount:'10,000',
+      date : new Date(2021,2,12)
+    },
+    {
+      id:3,
+      title:'scaler academy',
+      amount:'20,000',
+      date : new Date(2021,10,21)
+    },    
+    
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    < div className = 'App-header'>
+     <Expenses expenses = {expenses}></Expenses>
     </div>
   );
 }
